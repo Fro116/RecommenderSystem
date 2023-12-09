a0bf711eb9790fa8b6693dd774b12db3ce49256e
Fro116 <omnik116@gmail.com>
2023-12-08 19:12:45 -0500

    RecIngress: v9 support part 3

--- notebooks/RecIngress/CompressSplits.jl
+++ notebooks/RecIngress/CompressSplits.jl
@@ -1,41 +1,85 @@
+using CSV
+using DataFrames
+using JLD2
+using Memoize
+
 if !@isdefined COMPRESS_SPLITS_IFNDEF
     COMPRESS_SPLITS_IFNDEF = true
-    using CSV
-    using DataFrames
-    using JLD2    
-    struct RatingsDataset
-        user::Vector{Int32}
-        item::Vector{Int32}
-        rating::Vector{Float32}
-        timestamp::Vector{Float32}
-        status::Vector{Int32}
-        completion::Vector{Float32}
-        source::Vector{Int32}
-        medium::String        
-    end    
-    
-    function get_dataset(file, medium)
-        df = DataFrame(CSV.File(file))
-        RatingsDataset(
-            fill(1, length(df.username)), # julia is 1 indexed
-            df[:, "$(medium)id"] .+ 1, # julia is 1 indexed
-            df.score,
-            df.timestamp,
-            df.status,
-            df.completion,
-            df.source,
-            medium
+
+    function split_save(file, values)
+        # save in multiple files to allow multithreaded reading
+        for (k, v) in values
+            JLD2.save("$file.$k.jld2", Dict(k => v), compress = true)
+        end
+    end
+
+    @memoize function num_items(medium)
+        df = DataFrame(CSV.File("../../data/processed_data/$(medium)_to_uid.csv"))
+        length(df.uid)
+    end
+
+    function get_dataset(inference, medium, file)
+        data = Dict(
+            "source" => Int32[],
+            "medium" => Int32[],
+            "userid" => Int32[],
+            "mediaid" => Int32[],
+            "status" => Int32[],
+            "rating" => Float32[],
+            "order" => Int32[],
+            "updated_at" => Float32[],
+            "created_at" => Float32[],
+            "started_at" => Float32[],
+            "finished_at" => Float32[],
+            "progress" => Float32[],
+            "repeat_count" => Int32[],
+            "priority" => Float32[],
+            "sentiment" => Int32[],
+            "sentiment_score" => Float32[],
+            "owned" => Float32[],
         )
-    end    
-    
-    dir = "../../data/recommendations"    
-end;
+
+        @assert inference == isnothing(file)
+        if inference
+            data["userid"] = Int32[0 for _ = 1:num_items(medium)]
+            data["medium"] = Int32.(1:num_items(medium))
+        else
+            type_parser =
+                (_, name) ->
+                    String(name) in keys(data) ? eltype(data[String(name)]) : Float32
+            # load in chunks to reduce memory usage
+            df = DataFrame(CSV.File(file, types = type_parser))
+            for k in keys(data)
+                append!(data[k], df[:, k])
+            end
+        end
+
+        # rename columns
+        data["itemid"] = data["mediaid"]
+        data["update_order"] = data["order"]
+        delete!(data, "mediaid")
+        delete!(data, "order")
+        data
+    end
+
+    function save_dataset(username, medium)
+        dir = "../../data/recommendations"
+        for split in ["rec_training", "rec_inference"]
+            stem = "$dir/$username/$medium.$split"
+            if split == "rec_training"
+                dataset =
+                    get_dataset(false, medium, "$dir/$username/user_$(medium)_list.csv")
+            elseif split == "rec_inference"
+                dataset = get_dataset(true, medium, nothing)
+            else
+                @assert false
+            end
+            split_save("$dir/$username/$split.$medium", dataset)
+        end
+    end
+end
 
 username = ARGS[1]
-for medium in ["anime", "manga"]
-    for content in ["explicit", "implicit", "ptw"]
-        stem = "$dir/$username/$medium.$content"
-        dataset = get_dataset("$stem.csv", medium)
-        jldsave("$stem.jld2"; dataset)
-    end
+for medium in ["manga", "anime"]
+    save_dataset(username, medium)
 end
