851f8f111d77c6574cfb13a6a5a7576e619a51bc
Fro116 <omnik116@gmail.com>
2024-11-03 19:28:52 -0500

    TrainingAlphas: use test_input and test_output splits for Simple and Baseline

--- notebooks/TrainingAlphas/Baseline/get_user_biases.jl
+++ notebooks/TrainingAlphas/Baseline/get_user_biases.jl
@@ -1,16 +1,18 @@
 function get_user_biases(df, params)
-    μ_a, λ_u, λ_a, λ_wu, λ_wa, λ_wt = params["λ"]
+    get_user_biases(df, params["λ"], params["a"], params["a_counts"])
+end;
+
+function get_user_biases(df, λ, a, item_counts)
+    μ_a, λ_u, λ_a, λ_wu, λ_wa, λ_wt = λ
     λ_u, λ_a = exp.((λ_u, λ_a))
     λ_wt = sigmoid(λ_wt)
-    a = params["a"]
-    item_counts = params["a_counts"]
 
     function get_user_partition(users, threadid, num_threads)
         [i for i in 1:length(users) if (users[i] % num_threads) + 1 == threadid]
     end
     userids = Set(df.userid)
-    user_bias = Dict{Int32,Float32}(x => 0 for x in userids)
-    denom = Dict{Int32,Float32}(x => λ_u for x in userids)
+    user_bias = Dict{Int32,eltype(a)}(x => zero(eltype(a)) for x in userids)
+    denom = Dict{Int32,typeof(λ_u)}(x => λ_u for x in userids)
     T = Threads.nthreads()
     @sync for t = 1:T
         Threads.@spawn begin
