dafa188eeb5e236ca5a8f500aea97ba5bf337195
Fro116 <omnik116@gmail.com>
2025-01-07 00:34:04 -0500

    Training: send compressed files

--- notebooks/Training/save_fingerprints.sql
+++ notebooks/Training/save_fingerprints.sql
@@ -1,9 +1,9 @@
 DROP TABLE IF EXISTS collect_users_staging;
 CREATE TABLE collect_users_staging (LIKE collect_users INCLUDING ALL);
-\copy collect_users_staging FROM '../../data/users/mal.fingerprints.csv' CSV HEADER;
-\copy collect_users_staging FROM '../../data/users/anilist.fingerprints.csv' CSV HEADER;
-\copy collect_users_staging FROM '../../data/users/kitsu.fingerprints.csv' CSV HEADER;
 \copy collect_users_staging FROM PROGRAM 'zstd -cdq ../../data/users/animeplanet.fingerprints.csv.zst' CSV HEADER;
+\copy collect_users_staging FROM PROGRAM 'zstd -cdq ../../data/users/kitsu.fingerprints.csv.zst' CSV HEADER;
+\copy collect_users_staging FROM PROGRAM 'zstd -cdq ../../data/users/anilist.fingerprints.csv.zst' CSV HEADER;
+\copy collect_users_staging FROM PROGRAM 'zstd -cdq ../../data/users/mal.fingerprints.csv.zst' CSV HEADER;
 
 BEGIN;
     LOCK TABLE collect_users IN ACCESS EXCLUSIVE MODE;
