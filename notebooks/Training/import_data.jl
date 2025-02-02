238cb3a03fddb7f3247f0f6d9a1f9f200e82210d
Fro116 <omnik116@gmail.com>
2025-02-02 10:53:03 -0500

    Training: import data

---
+++ notebooks/Training/import_data.jl
@@ -0,0 +1,150 @@
+import CodecZstd
+import CSV
+import DataFrames
+import Glob
+import Memoize: @memoize
+import MsgPack
+import Random
+import ProgressMeter: @showprogress
+include("../julia_utils/stdout.jl")
+include("import_list.jl")
+
+const MEDIUMS = ["manga", "anime"]
+const SOURCES = ["mal", "anilist", "kitsu", "animeplanet"]
+const datadir = "../../data/training"
+const envdir = "../../environment"
+
+function download_data()
+    rm(datadir, force = true, recursive = true)
+    mkpath(datadir)
+    retrieval = read("$envdir/database/retrieval.txt", String)
+    files = vcat(
+        ["$m.groups.csv" for m in MEDIUMS],
+        ["$(s)_$(m).csv" for s in SOURCES for m in MEDIUMS],
+        ["fingerprints.csv"],
+    )
+    for fn in files
+        cmd = "$retrieval/$fn $datadir/$fn"
+        run(`sh -c $cmd`)
+    end
+    run(
+        `mlr --csv split -n 1000000 --prefix $datadir/fingerprints $datadir/fingerprints.csv`,
+    )
+    rm("$datadir/fingerprints.csv")
+end
+
+@memoize function get_media_length(source, medium)
+    fn = "$datadir/$(source)_$(medium).csv"
+    df = CSV.read(fn, DataFrames.DataFrame)
+    rm(fn)
+    parseint(x::Missing) = nothing
+    parseint(x::Real) = x
+    parseint(x::AbstractString) = parse(Int, replace(x, "+" => ""))
+    Dict(
+        string(uid) => Dict(
+            "episodes" => parseint(eps),
+            "chapters" => parseint(chs),
+            "volumes" => parseint(vls),
+        ) for (uid, eps, chs, vls) in zip(df.itemid, df.episodes, df.chapters, df.volumes)
+    )
+end
+
+function shuffle_col!(df, col)
+    defaultval = 0
+    vals = Set(df[:, col])
+    delete!(vals, defaultval)
+    remap = Random.shuffle(collect(vals))
+    remap = Dict(x => i for (i, x) in Iterators.enumerate(remap))
+    remap[defaultval] = defaultval
+    df[:, col] .= map(x -> remap[x], df[:, col])
+end
+
+@memoize function get_media_groups(medium)
+    fn = "$datadir/$medium.groups.csv"
+    df = CSV.read(
+        fn,
+        DataFrames.DataFrame,
+        types = Dict("itemid" => String),
+    )
+    rm(fn)
+    cols = [:episodes, :chapters, :volumes]
+    for c in cols
+        df[!, c] .= 0
+    end
+    for i = 1:DataFrames.nrow(df)
+        lens = get(get_media_length(df.source[i], medium), df.itemid[i], Dict())
+        for c in cols
+            df[i, c] = something(get(lens, string(c), nothing), 0)
+        end
+    end
+    sort!(df, :count, rev = true)
+    df[!, :distinctid] .= 0
+    df[!, :matchedid] .= 0
+    min_count = 100
+    distinctid = 0
+    groupmap = Dict()
+    for i = 1:DataFrames.nrow(df)
+        if df.count[i] < min_count
+            df[i, :distinctid] = 0
+            df[i, :matchedid] = get(groupmap, df[i, :groupid], 0)
+        else
+            distinctid += 1
+            if df[i, :groupid] ∉ keys(groupmap)
+                groupmap[df[i, :groupid]] = length(groupmap) + 1
+            end
+            df[i, :distinctid] = distinctid
+            df[i, :matchedid] = groupmap[df[i, :groupid]]
+        end
+    end
+    for c in [:distinctid, :matchedid]
+        shuffle_col!(df, c)
+    end
+    df
+end
+
+function gen_splits()
+    min_items = 5
+    test_perc = 0.01
+    rm("$datadir/users", recursive = true, force = true)
+    @showprogress for (idx, f) in
+                      Iterators.enumerate(Glob.glob("$datadir/fingerprints_*.csv"))
+        train_dir = "$datadir/users/training/$idx"
+        test_dir = "$datadir/users/test/$idx"
+        mkpath.([train_dir, test_dir])
+        df = Random.shuffle(read_csv(f))
+        Threads.@threads for i = 1:DataFrames.nrow(df)
+            user = import_user(df.source[i], decompress(df.data[i]))
+            if length(user["items"]) < min_items
+                continue
+            end
+            outdir = rand() < test_perc ? test_dir : train_dir
+            open("$outdir/$i.msgpack", "w") do g
+                write(g, MsgPack.pack(user))
+            end
+        end
+        rm(f)
+    end
+end
+
+function import_data()
+    download_data()
+    for m in MEDIUMS
+        CSV.write("$datadir/$m.csv", get_media_groups(m))
+    end
+    date = Dates.format(Dates.today(), "yyyymmdd")
+    open("$datadir/latest", "w") do f
+        write(f, date)
+    end
+    save_template = read("$envdir/database/storage.txt", String)
+    for m in MEDIUMS
+        cmd = replace(
+            save_template,
+            "{INPUT}" => "$datadir/$m.csv",
+            "{OUTPUT}" => "$date/$m.csv",
+        )
+        run(`sh -c $cmd`)
+    end
+    gen_splits()
+end
+
+import_data()
---
+++ notebooks/Training/import_list.jl
@@ -0,0 +1,272 @@
+import CSV
+import Dates
+import DataFrames
+import Memoize: @memoize
+
+const STATUS_MAP = Dict{String,Int32}(
+    "rewatching" => 7,
+    "completed" => 6,
+    "currently_watching" => 5,
+    "on_hold" => 4,
+    "planned" => 3,
+    "dropped" => 2,
+    "wont_watch" => 1,
+    "none" => 0,
+)
+
+const MEDIUM_MAP = Dict{String,Int32}("manga" => 0, "anime" => 1)
+
+const mediadir = "../../data/training"
+
+@memoize function get_media(medium)
+    df = CSV.read(
+        "$mediadir/$medium.csv",
+        DataFrames.DataFrame,
+        types = Dict("itemid" => String),
+    )
+    d = Dict()
+    for i = 1:DataFrames.nrow(df)
+        d[(df.source[i], df.itemid[i])] = Dict(
+            "episodes" => df.episodes[i],
+            "chapters" => df.chapters[i],
+            "volumes" => df.volumes[i],
+            "distinctid" => df.distinctid[i],
+            "matchedid" => df.matchedid[i],
+        )
+    end
+    d
+end
+
+function read_csv(fn)
+    # CSV.read can't handle rows with >1m characters
+    lines = readlines(fn)
+    rows = Vector{Any}(undef, length(lines))
+    Threads.@threads for i = 1:length(lines)
+        rows[i] = split(lines[i], r",(?=(?:[^\"]*\"[^\"]*\")*[^\"]*$)")
+    end
+    colnames = rows[1]
+    records = rows[2:end]
+    df = DataFrames.DataFrame(
+        Dict(colnames[i] => [x[i] for x in records] for i = 1:length(colnames)),
+    )
+    df[:, colnames]
+end
+
+function decompress(x)
+    MsgPack.unpack(
+        CodecZstd.transcode(CodecZstd.ZstdDecompressor, Vector{UInt8}(hex2bytes(x[3:end]))),
+    )
+end
+
+function get_progress(source, medium, itemid, episodes, chapters, volumes)::Float32
+    df = get_media(medium)
+    k = (source, string(itemid))
+    if k ∉ keys(df)
+        return 0
+    end
+    isvalid(x) = !ismissing(x) && !isnothing(x) && x != 0
+    progress = 0
+    if isvalid(episodes) && isvalid(df[k]["episodes"])
+        progress = max(progress, episodes / df[k]["episodes"])
+    elseif isvalid(chapters) && isvalid(df[k]["chapters"])
+        progress = max(progress, chapters / df[k]["chapters"])
+    elseif isvalid(volumes) && isvalid(df[k]["volumes"])
+        progress = max(progress, volumes / df[k]["volumes"])
+    end
+    min(progress, 1.0)
+end
+
+function create_item!(source, medium, itemid, item)
+    if item["medium"] ∉ values(MEDIUM_MAP)
+        logerror("invalid medium for $item")
+        item["medium"] = 0
+    end
+    if item["status"] ∉ values(STATUS_MAP)
+        logerror("invalid status for $item")
+        item["status"] = 0
+    end
+    if item["rating"] > 10 || item["rating"] < 0
+        logerror("invalid rating for $item")
+        item["rating"] = 0
+    end
+    if item["update_order"] < 0
+        logerror("invalid update_order for $item")
+        item["update_order"] = 0
+    end
+    if item["updated_at"] <= 0
+        item["updated_at"] = 0
+    elseif item["updated_at"] < Dates.datetime2unix(Dates.DateTime(2000, 1, 1)) ||
+           item["updated_at"] > time() + 86400
+        logerror("invalid updated_at for $item")
+        item["updated_at"] = 0
+    end
+    if item["progress"] < 0 || item["progress"] > 1
+        logerror("invalid progress for $item")
+        item["progress"] = 0
+    end
+    d = get(get_media(medium), (source, string(itemid)), Dict())
+    item["distinctid"] = get(d, "distinctid", 0)
+    item["matchedid"] = get(d, "matchedid", 0)
+end
+
+function import_mal_user(data)
+    source = "mal"
+    user = Dict("source" => source)
+    items = []
+    status_map = Dict(
+        "completed" => "completed",
+        "watching" => "currently_watching",
+        "plan_to_watch" => "planned",
+        "reading" => "currently_watching",
+        "plan_to_read" => "planned",
+        "on_hold" => "on_hold",
+        "dropped" => "dropped",
+        nothing => "none",
+    )
+    for x in data["items"]
+        item = Dict(
+            "medium" => MEDIUM_MAP[x["medium"]],
+            "status" => STATUS_MAP[status_map[x["status"]]],
+            "rating" => convert(Int32, x["score"]),
+            "updated_at" =>
+                isnothing(x["updated_at"]) ? 0 :
+                Dates.datetime2unix(
+                    Dates.DateTime(x["updated_at"], "yyyy-mm-ddTHH:MM:SS+00:00"),
+                ),
+            "update_order" => 0,
+            "progress" => get_progress(
+                source,
+                x["medium"],
+                x["itemid"],
+                x["progress"],
+                x["progress"],
+                x["num_volumes_read"],
+            ),
+        )
+        create_item!(source, x["medium"], x["itemid"], item)
+        push!(items, item)
+    end
+    Dict("user" => user, "items" => items)
+end
+
+function import_anilist_user(data)
+    source = "anilist"
+    user = Dict("source" => source)
+    items = []
+    status_map = Dict(
+        "REPEATING" => "rewatching",
+        "COMPLETED" => "completed",
+        "CURRENT" => "currently_watching",
+        "PLANNING" => "planned",
+        "PAUSED" => "on_hold",
+        "DROPPED" => "dropped",
+        nothing => "none",
+    )
+    for x in data["items"]
+        item = Dict(
+            "medium" => MEDIUM_MAP[x["medium"]],
+            "status" => STATUS_MAP[status_map[x["status"]]],
+            "rating" => convert(Float32, x["score"]),
+            "updated_at" => convert(Float32, something(x["updatedat"], 0)),
+            "update_order" => 0,
+            "progress" => get_progress(
+                source,
+                x["medium"],
+                x["itemid"],
+                x["progress"],
+                x["progress"],
+                x["progressvolumes"],
+            ),
+        )
+        create_item!(source, x["medium"], x["itemid"], item)
+        push!(items, item)
+    end
+    Dict("user" => user, "items" => items)
+end
+
+function import_kitsu_user(data)
+    source = "kitsu"
+    user = Dict("source" => source)
+    items = []
+    status_map = Dict(
+        "completed" => "completed",
+        "current" => "currently_watching",
+        "dropped" => "dropped",
+        "on_hold" => "on_hold",
+        "planned" => "planned",
+        nothing => "none",
+    )
+    for x in data["items"]
+        item = Dict(
+            "medium" => MEDIUM_MAP[x["medium"]],
+            "status" => STATUS_MAP[status_map[x["status"]]],
+            "rating" => convert(Float32, something(x["ratingtwenty"], 0)) / 2,
+            "updated_at" =>
+                isnothing(x["updatedat"]) ? 0 :
+                Dates.datetime2unix(
+                    Dates.DateTime(x["updatedat"], "yyyy-mm-ddTHH:MM:SS.sssZ"),
+                ),
+            "update_order" => 0,
+            "progress" => get_progress(
+                source,
+                x["medium"],
+                x["itemid"],
+                x["progress"],
+                x["progress"],
+                nothing,
+            ),
+        )
+        create_item!(source, x["medium"], x["itemid"], item)
+        push!(items, item)
+    end
+    Dict("user" => user, "items" => items)
+end
+
+function import_animeplanet_user(data)
+    source = "animeplanet"
+    user = Dict("source" => source)
+    items = []
+    status_map = Dict(
+        1 => "completed",
+        2 => "currently_watching",
+        3 => "dropped",
+        4 => "planned",
+        5 => "on_hold",
+        6 => "wont_watch",
+        nothing => "none",
+    )
+    for x in data["items"]
+        item = Dict(
+            "medium" => MEDIUM_MAP[x["medium"]],
+            "status" => STATUS_MAP[status_map[x["status"]]],
+            "rating" => convert(Float32, something(x["score"], 0)) * 2,
+            "updated_at" => something(x["updated_at"], 0),
+            "update_order" => convert(Int32, something(x["item_order"], 0)),
+            "progress" => get_progress(
+                source,
+                x["medium"],
+                x["itemid"],
+                x["progress"],
+                x["progress"],
+                nothing,
+            ),
+        )
+        create_item!(source, x["medium"], x["itemid"], item)
+        push!(items, item)
+    end
+    Dict("user" => user, "items" => items)
+end
+
+function import_user(source, data)
+    if source == "mal"
+        return import_mal_user(data)
+    elseif source == "anilist"
+        return import_anilist_user(data)
+    elseif source == "kitsu"
+        return import_kitsu_user(data)
+    elseif source == "animeplanet"
+        return import_animeplanet_user(data)
+    else
+        @assert false
+    end
+end
